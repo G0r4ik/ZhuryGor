e3e59656cbb413db299d5efbe66edee1facedf49
G0r4ik <62844362+G0r4ik@users.noreply.github.com>
2020-06-30 16:17:35 +0300

    Add registration, img

--- main.js
+++ main.js
@@ -7,7 +7,20 @@ showModalUser.addEventListener("click" , () => {
 })
 
 hideModalUser.addEventListener("click" , () => {
-    modalUser.style.display = "none"
+    modalUser.style.display = "none";
 })
 
 // Диалоговое окно показать/убрать
+
+let showModalUserReg = document.getElementById("showModalUserReg")
+let modalUserReg = document.getElementById("modalUserReg")
+let hideModalUserReg = document.getElementById("hideModalUserReg")
+
+showModalUserReg.addEventListener("click" , () => {
+    modalUserReg.style.display = "block"
+    modalUser.style.display = "none";
+}) 
+
+hideModalUserReg.addEventListener("click" , () => {
+    modalUserReg.style.display = "none";
+})
